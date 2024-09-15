ebcbeba2d5e1408518edf8c3a86f751630715b84
unilimitclone <93084675+unilimitclone@users.noreply.github.com>
2024-09-15 14:04:53 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 # Set permissions and owners.
-chown -R ${PUID}:${PGID} /opt/alist/
-umask ${UMASK}
+#chown -R ${PUID}:${PGID} /opt/alist/
+#umask ${UMASK}
 # Create data folder.
-mkdir -p /opt/alist/data
+#mkdir -p /opt/alist/data
 # Config Alist
-/main
+#/main
 # main
-exec su-exec ${PUID}:${PGID} /usr/bin/supervisord -c /etc/supervisord.conf
+#exec su-exec ${PUID}:${PGID} /usr/bin/supervisord -c /etc/supervisord.conf
+
+export HTTP_PORT=${PORT}
+mkdir -p /opt/alist/data/
+/main
+
+cd /opt/alist
+./alist server --no-prefix
